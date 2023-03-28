944d3ce70513ef113dc7a41483a9e7c59458724f
NeoDau <neodau@gmail.com>
2023-03-28 07:44:23 -0700

    create server

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""My first application with flask."""
 from flask import Flask
+
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def say_hello():
-    """Return hello HBNB on /."""
-    return "Hello HBNB!"
+@app.route("/", strict_slashes=False)
+def index():
+    return("Hello HBNB!")
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
